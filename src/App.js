68cdfd30fbdf1db67f7a03db53203e87d2ada92d
EduPassero <edupassero47@gmail.com>
2022-06-05 14:42:33 -0300

    Criaçao do Costs
    Curso React - Hora de Codar

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import Container from './components/layout/Container';
+import Footer from './components/layout/Footer';
+import Navbar from './components/layout/Navbar';
+import Company from './components/pages/Company';
+import Contato from './components/pages/Contato';
+import Home from './components/pages/Home';
+import NewProject from './components/pages/NewProjetc';
+import Projects from './components/pages/Projects';
 
 function App() {
   return (
     <Router>
-      <ul>
-        <li>Home</li>
-        <li>Contato</li>
-      </ul>
-      <Routes>
-        <Route path="/" element=""/>
-        <Route path="/contato" element=""/>
-      </Routes>
+      <Navbar />
+      <Container customClass="min-height">
+        <Routes>
+            <Route path="/" element={<Home/>}/>
+            <Route path="projects" element={<Projects/>}/>
+            <Route path="contato" element={<Contato/>}/>
+            <Route path="company" element={<Company/>}/>
+            <Route path="newproject" element={<NewProject/>}/>
+        </Routes>
+      </Container>
+      <Footer />
     </Router>
   );
 }
